804392a168e00bdc924747e689c4eb2abfbf5002
Elijah Rosen <elijahdavidrosen@gmail.com>
2025-03-14 14:15:21 -0400

    renamed default port to ws port

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import { io } from "socket.io-client";
 import React from "react";
 
 const hostname = import.meta.env.VITE_HOST || window.location.hostname;
-const DEFAULT_PORT = 8080;
+const WS_PORT = 8080;
 const WS_ADDRESS = `ws://${hostname}:${DEFAULT_PORT}`;
 
 function App() {
