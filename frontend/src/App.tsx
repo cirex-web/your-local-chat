387e2af813b931eeb95e57cc23958ec6acfa545a
Elijah Rosen <elijahdavidrosen@gmail.com>
2025-03-14 14:13:31 -0400

    moved url code outside of app function

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,12 @@ import "./App.css";
 import { io } from "socket.io-client";
 import React from "react";
 
+
+const hostname = import.meta.env.VITE_HOST || window.location.hostname;
+const DEFAULT_PORT = 8080;
+const WS_ADDRESS = `ws://${hostname}:${DEFAULT_PORT}`;
+
 function App() {
-  const hostname = import.meta.env.VITE_HOST || window.location.hostname;
-  const DEFAULT_PORT = 8080;
-  const WS_ADDRESS = `ws://${hostname}:${DEFAULT_PORT}`;
   
   const socket = useMemo(() => io(WS_ADDRESS), []);
   const [isConnected, setIsConnected] = useState(socket.connected);
